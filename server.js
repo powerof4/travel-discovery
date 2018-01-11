ed1a80e4f43a7e66acda1d3bdfdf0fdb0cacdd80
Tiffany Thompson <tdthompson899@gmail.com>
2018-01-11 11:33:20 -0600

    save the questions from the form to the db and render new page

--- server.js
+++ server.js
@@ -20,12 +20,31 @@ var User = require("./models/users");
 var Question = require("./models/questions");
 var Answer = require("./models/answers");
 
+// load the index page
 app.get('/', function(req, res){
   res.render('pages/index');
 });
 
-app.get('/questions', function(req, res){
-  res.render('pages/questions');
+// load view - questions for a form to add questions
+app.get('/admin/questions', function(req, res){
+  res.render('pages/admin/questions');
+});
+
+// save the data from the form
+app.post('/saveQuestion', function(req, res){
+  var newQuest = new Question({
+      title: req.body.quest_title,
+      image: req.body.img_file
+  });
+
+  // save the new question then render the all page, catch any errors and respond with error
+  newQuest.save().then(data => {
+      console.log(data);
+      res.render('pages/admin/all');
+  }).catch(err => {
+      res.status(400).send('Unable to save the question to the database');
+    }
+  );
 });
 
 app.listen(port, function(){
